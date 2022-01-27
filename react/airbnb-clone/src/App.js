02a61694d93f7c4444ee83036ee7cd8233b26c3b
asknishant <asknishant.39@gmail.com>
2022-01-27 20:26:31 +0530

    First commit

---
+++ react/airbnb-clone/src/App.js
@@ -0,0 +1,19 @@
+import NavBar from './components/Navbar';
+import Hero from './components/Hero'
+import Card from './components/Card';
+
+
+import './App.css';
+import './styles.css'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Card />
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ react/airbnb-clone/src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../images/logo.jpg'
+export default function Card() {
+    return (
+        <div className='card'>
+            <img src={logo} className='card--image'/>
+            <div>
+                <span>5.0</span>
+                <span>(6) </span>
+                <span>USA</span>
+            </div>
+            <p>Lifr Lessons with Katie Zaferes</p>
+            <p>From $136 / person</p>
+        </div>
+    )
+}
